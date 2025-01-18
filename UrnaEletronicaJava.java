7d86dcda5d3f414bb992d89bdcc092a3f3a52e4c
andrelzl <andreluizcar399@gmail.com>
2025-01-18 14:50:24 -0300

    codigo final

--- UrnaEletronicaJava.java
+++ UrnaEletronicaJava.java
@@ -7,9 +7,7 @@ class UrnaEletronica{
     private int votosNulo;
     private boolean nenhumVotoValido;
             
-            //metodo para configurar a urna com os candidatos
-            public void configUrnaEletronica(){                   
-                
+            public void configUrnaEletronica(){                                  
                 Candidato candidato01 = new Candidato("Ada Lovelace", 1);
                 Candidato candidato02 = new Candidato("Alan Turing", 2);
                 Candidato candidato03 = new Candidato("Marie Curie", 3);
@@ -22,7 +20,6 @@ class UrnaEletronica{
                 arrayCandidatos.add(candidato05);        
             }
             
-            //Metodo para inserir vodo na array do candidato 
             public void adicionarVoto(int numero){
                 if (numero < 1 || numero > 5){     
                     votosNulo++;           
@@ -36,72 +33,70 @@ class UrnaEletronica{
                 }                
             }
 
-            //Faz a apuração do vencedor 
             public ArrayList<String> apurarVotos(){
                 int maxVotos = 0; 
-                ArrayList<String> vencedor = new ArrayList<>(); 
+                ArrayList<String> resultadoFinal = new ArrayList<>(); 
                 if (votosNulo == 10){
                     nenhumVotoValido = true;
                 }else{
                     for (Candidato candidato : arrayCandidatos) {
                         if (candidato.getVotos() > maxVotos){   
                             maxVotos = candidato.getVotos();
-                            vencedor.clear();
-                            vencedor.add(candidato.getNome());
+                            resultadoFinal.clear();
+                            resultadoFinal.add(candidato.getNome());
                         }else if(candidato.getVotos() == maxVotos && maxVotos > 0){
-                            vencedor.add(candidato.getNome());
+                            resultadoFinal.add(candidato.getNome());
                         }
                     }
                 }
-                return vencedor;
+                return resultadoFinal;
             }
 
-            //Metodo para divulgar o resultado
-            public void divulgarResultado(ArrayList<String> vencedor){
+            public void divulgarResultado(ArrayList<String> resultadoFinal){
                 double porcentagem = 0;
-                String nome;
+                String nomeCandidato;
                 int votos;
                 if(nenhumVotoValido){
                     System.out.println("Resultado da Votação: \n");
-                    System.out.println("Nenhum candidato teve voto nessa eleição!");
+                    System.out.println("Nenhum candidato teve votos nesta eleição!");
                     for (Candidato candidato : arrayCandidatos){
-                        nome = candidato.getNome();
+                        nomeCandidato = candidato.getNome();
                         votos = candidato.getVotos();
                         porcentagem = candidato.getVotos()*10;
-                        System.out.println(nome + ":" + votos + " (" +porcentagem +")");
+                        System.out.println(nomeCandidato + ":" + votos + " (" +porcentagem +")");
                     }
                     System.out.println("Votos nulos: " + votosNulo);
-                }else if(vencedor.size() > 1){
+                }else if(resultadoFinal.size() > 1){
                     System.out.println("Resultado da Votação: \n");
                     for (Candidato candidato : arrayCandidatos){
-                        nome = candidato.getNome();
+                        nomeCandidato = candidato.getNome();
                         votos = candidato.getVotos();
                         porcentagem = candidato.getVotos()*10;
                         if (votos == 1){
-                            System.out.println(nome + ": " + votos + " voto (" +porcentagem +"%)");
+                            System.out.println(nomeCandidato + ": " + votos + " voto (" +porcentagem +"%)");
                         }else{
-                            System.out.println(nome + ": " + votos + " votos (" +porcentagem +"%)");
+                            System.out.println(nomeCandidato + ": " + votos + " votos (" +porcentagem +"%)");
                         }
                     }
                     System.out.println("Votos nulos: " + votosNulo + "\n");
                     System.out.println("Houve empate entre os candidatos: \n");
-                    for (String string : vencedor) {
+                    for (String string : resultadoFinal) {
                         System.out.println(string);
                     }                      
                 }else{    
                     System.out.println("Resultado da Votação: \n");
                     for (Candidato candidato : arrayCandidatos){
-                        nome = candidato.getNome();
+                        nomeCandidato = candidato.getNome();
                         votos = candidato.getVotos();
                         porcentagem = candidato.getVotos()*10;
                         if (votos == 1){
-                            System.out.println(nome + ": " + votos + " voto (" +porcentagem +"%)");
+                            System.out.println(nomeCandidato + ": " + votos + " voto (" +porcentagem +"%)");
                         }else{
-                            System.out.println(nome + ": " + votos + " votos (" +porcentagem +"%)");
+                            System.out.println(nomeCandidato + ": " + votos + " votos (" +porcentagem +"%)");
                         }
                     }
                     System.out.println("Votos nulos: " + votosNulo);
-                    System.out.println("Vencedor: " + vencedor.get(0));         
+                    System.out.println("Vencedor: " + resultadoFinal.get(0));         
                 }
             }
 }
@@ -118,21 +113,19 @@ public class UrnaEletronicaJava {
                         "03 - Marie Curie\r\n" + //
                         "04 - Albert Einstein\r\n" + //
                         "05 - Ludwig van Beethoven");
-        //Laço para receber os votos
+
         for (int i = 0; i < 10; i++){
             System.out.println("Digite o número do seu candidato:");
             String entrada = scanner.nextLine();
-            /*Verificar se o numero de 01 até 05 foi digitado corretamente
-            caso seja digitado 1 ou 5, retorna entrada invalida e pede novamente*/
-            if (entrada.equals("1") || entrada.equals("2") || entrada.equals("3") || entrada.equals("4") || entrada.equals("5")){
+            if (entrada.equals("1") || entrada.equals("2") || entrada.equals("3") || entrada.equals("4") || entrada.equals("5") || entrada.equals("")){
                 System.out.println("Entrada invalida, tente novamente!");
                 i--;
             }else{
                 if (entrada.length() < 2 || entrada.length() > 2){
                     urna.adicionarVoto(0);
                 }else{
-                    int numeroConverter = Integer.parseInt(entrada);
-                    urna.adicionarVoto(numeroConverter);
+                    int numeroConvertido = Integer.parseInt(entrada);
+                    urna.adicionarVoto(numeroConvertido);
                 }
             }
         }
